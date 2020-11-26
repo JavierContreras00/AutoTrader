e87330da291c664d082a549909ff6f33fb59de63
javic <javic@DESKTOP-FONOMVE>
2020-11-26 15:24:33 +0100

    Nuevas ventanas

---
+++ AutoTrader/src/ventanas/VentanaAddVehiculo.java
@@ -0,0 +1,10 @@
+package ventanas;
+
+public class VentanaAddVehiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AutoTrader/src/ventanas/VentanaPrograma.java
@@ -0,0 +1,10 @@
+package ventanas;
+
+public class VentanaPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
