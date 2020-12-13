44c12735a51279dae997d44b2e51fec35b046bdb
SAO <SAO@N04450>
2020-12-13 11:45:44 +0100

    Test comprobar correo

--- AutoTrader/src/Test/TestUsuario.java
+++ AutoTrader/src/Test/TestUsuario.java
@@ -6,5 +6,10 @@ import org.junit.Test;
 import ventanas.VentanaRegistrarse;
 
 public class TestUsuario {
+	@Test
+	public void correoTest() {
+		assertTrue(VentanaRegistrarse.comprobarCorreo("santiago.alfaro@opendeusto.es", false));
+		assertFalse(VentanaRegistrarse.comprobarCorreo("santiago.alfaro@opendeusto.es", false));
+	}
 
 }
